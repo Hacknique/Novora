b4c218f483f1ad1368a5689d0855fe1a1cf55702
James David Clarke <james@jamesdavidclarke.com>
2023-10-19 18:42:08 +0100

    Made it so World created, Are Listed in the Main Menu

--- scripts/Menus/MainMenu/WorldList.gd
+++ scripts/Menus/MainMenu/WorldList.gd
@@ -1,9 +1,16 @@
-extends Node
+extends ItemList
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	const base_path = "user://"
+	const worlds_path = base_path + "worlds/"
+
+	var worlds = DirAccess.get_files_at(worlds_path)
+	
+	for name in worlds:
+		name = name.trim_suffix(".tres")
+		self.add_item(name)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
