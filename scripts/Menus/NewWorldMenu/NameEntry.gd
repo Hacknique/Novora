1f60a1542646822fb52dde84d0ea466e487a074e
James David Clarke <james@jamesdavidclarke.com>
2023-10-20 14:26:30 +0100

    Added report template

--- scripts/Menus/NewWorldMenu/NameEntry.gd
+++ scripts/Menus/NewWorldMenu/NameEntry.gd
@@ -1,7 +1,8 @@
 extends Node
 
 func _on_text_changed(new_text):
-	if new_text != "":
+	var world = World.new(new_text)
+	if new_text != "" and !world.does_exist() and %OptionButton.selected != -1:
 		%CreateNewWorldBtn.disabled = false
 	else:
 		%CreateNewWorldBtn.disabled = true
