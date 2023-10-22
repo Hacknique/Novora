a2b6cbed123a07d65e8b525517995438afff3ee2
James David Clarke <james@jamesdavidclarke.com>
2023-10-22 17:44:23 +0100

    Fixed Lighting issue with Voxels and started on black border for voxels.

--- scripts/Objects/Voxel.gd
+++ scripts/Objects/Voxel.gd
@@ -1,12 +1,14 @@
 extends StaticBody3D
 class_name Voxel
 
+## Sets The Atlas Texture for the Voxel
 func set_texture(atlas_texture: String):
-	var material = self.get_node("Texture").get_active_material(0)  # Assuming the material is on surface 0
+	var material = self.get_node("Texture").get_active_material(0) 
 	
 	var tex = load(atlas_texture)
 	material.set_texture(0, tex)
 
 
 func _ready():
-	set_texture("res://assets/Voxel/voxel_texture_atlas.svg")
+	set_texture("res://assets/Voxel/voxel_texture_atlas.webp")
+	
