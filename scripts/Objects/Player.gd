06e5fa56aee80b81a2ea774459928fc3da7c032c
James David Clarke <james@jamesdavidclarke.com>
2023-10-29 12:25:15 +0000

    Added back the pause menu.

--- scripts/Objects/Player.gd
+++ scripts/Objects/Player.gd
@@ -20,44 +20,57 @@ var _movement_speed: float = _speed_walk
 @onready var neck: Node3D = self.get_node("CharacterBody3D/CameraPivot")
 @onready var eyes: Camera3D = self.get_node("CharacterBody3D/CameraPivot/Camera3D")
 
+# Preload the escape menu scene
+var escape_menu_scene = preload("res://scenes/World/UserInterface/EscapeMenu.tscn")
+var escape_menu_instance = null
+
+func toggle_escape_menu():
+	if !escape_menu_instance:
+		# Instance and add the escape menu to the scene
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		escape_menu_instance = escape_menu_scene.instantiate()
+		self.add_child(escape_menu_instance)
+	else:
+		# If already present, free the instance and set the variable to null
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		escape_menu_instance.queue_free()
+		escape_menu_instance = null
+
 func _ready():
-    Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _process(delta):
-    if Input.is_action_just_pressed("ui_cancel"):
-        match (Input.mouse_mode):
-            Input.MOUSE_MODE_CAPTURED:
-                Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-            _:
-                Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	if Input.is_action_just_pressed("ui_cancel"):
+		toggle_escape_menu()
+			
 
 func _physics_process(delta):
-    self.body.move_and_slide()
+	self.body.move_and_slide()
 
-    if Input.is_action_pressed("p_sprint"):
-        self._movement_speed = self._speed_sprint
-    elif Input.is_action_pressed("p_crouch"):
-        self._movement_speed = self._speed_crouch
-    else:
-        self._movement_speed = self._speed_walk
+	if Input.is_action_pressed("p_sprint"):
+		self._movement_speed = self._speed_sprint
+	elif Input.is_action_pressed("p_crouch"):
+		self._movement_speed = self._speed_crouch
+	else:
+		self._movement_speed = self._speed_walk
 
-    if not self.body.is_on_floor():
-        self.body.velocity.y += (-self._gravity * 2) * delta
+	if not self.body.is_on_floor():
+		self.body.velocity.y += (-self._gravity * 2) * delta
 
-    if Input.is_action_pressed("p_jump") and self.body.is_on_floor():
-        self.body.velocity.y = _jump_force
+	if Input.is_action_pressed("p_jump") and self.body.is_on_floor():
+		self.body.velocity.y = _jump_force
 
-    var input_dir = Input.get_vector("p_left", "p_right", "p_forward", "p_backward")
-    var direction = (self.body.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-    if direction:
-        self.body.velocity.x = direction.x * _movement_speed
-        self.body.velocity.z = direction.z * _movement_speed
-    else:
-        self.body.velocity.x = 0
-        self.body.velocity.z = 0
+	var input_dir = Input.get_vector("p_left", "p_right", "p_forward", "p_backward")
+	var direction = (self.body.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		self.body.velocity.x = direction.x * _movement_speed
+		self.body.velocity.z = direction.z * _movement_speed
+	else:
+		self.body.velocity.x = 0
+		self.body.velocity.z = 0
 
 func _input(event):
-    if event is InputEventMouseMotion:
-        self.body.rotate_y(deg_to_rad(-event.relative.x * _mouse_sensitivity))
-        self.neck.rotate_x(deg_to_rad(-event.relative.y * _mouse_sensitivity))
-        self.neck.rotation.x = clampf(self.neck.rotation.x, deg_to_rad(-89), deg_to_rad(89))
+	if event is InputEventMouseMotion and !escape_menu_instance:
+		self.body.rotate_y(deg_to_rad(-event.relative.x * _mouse_sensitivity))
+		self.neck.rotate_x(deg_to_rad(-event.relative.y * _mouse_sensitivity))
+		self.neck.rotation.x = clampf(self.neck.rotation.x, deg_to_rad(-89), deg_to_rad(89))
