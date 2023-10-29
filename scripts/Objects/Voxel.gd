fc90537a0c5f93adbda873a9077811beff455708
nitrogenez <nitrogenesis.developer@gmail.com>
2023-10-29 13:47:42 +0200

    updated player, added new actions to input map, etc.

--- scripts/Objects/Voxel.gd
+++ scripts/Objects/Voxel.gd
@@ -3,12 +3,12 @@ class_name Voxel
 
 ## Sets The Atlas Texture for the Voxel
 func set_texture(atlas_texture: String):
-	var material = self.get_node("Texture").get_active_material(0) 
-	
-	var tex = load(atlas_texture)
-	material.set_shader_parameter("atlas_texture", tex)
+    var material = self.get_node("Texture").get_active_material(0)
+
+    var tex = load(atlas_texture)
+    material.set_shader_parameter("atlas_texture", tex)
 
 
 func _ready():
-	set_texture("res://assets/Voxel/voxel_texture_atlas.webp")
-	
+    set_texture("res://assets/Voxel/voxel_texture_atlas.webp")
+
--- scripts/World/World.gd
+++ scripts/World/World.gd
@@ -5,14 +5,14 @@ var world_data
 var VoxelScene = preload("res://scenes/Objects/Voxel.tscn")
 
 func _on_ready():
-	self.world_name = Game.current_world_name
-	print(self.world_name)
-	var world = World.new(self.world_name)
-	world_data = world.data
-	world.load_or_save()
-	
-	# Create an instance of the scene
-	var voxel_instance = VoxelScene.instantiate()
+    self.world_name = Game.current_world_name
+    print(self.world_name)
+    var world = World.new(self.world_name)
+    world_data = world.data
+    world.load_or_save()
 
-	# Add the instance to the current scene
-	add_child(voxel_instance)
+    # Create an instance of the scene
+    var voxel_instance = VoxelScene.instantiate()
+
+    # Add the instance to the current scene
+    add_child(voxel_instance)
