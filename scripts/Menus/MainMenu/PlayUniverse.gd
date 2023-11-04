67399f5783a2842c175dfd7261fb716807907a45
James David Clarke <james@jamesdavidclarke.com>
2023-11-04 13:21:30 +0000

    More Fixes

--- scripts/Menus/MainMenu/PlayUniverse.gd
+++ scripts/Menus/MainMenu/PlayUniverse.gd
@@ -1,7 +1,7 @@
 extends Node
 
 func _on_pressed():
-	var selected_index = %WorldList.get_selected_items()[0]  # This gives an array of selected item indices, [0] gets the first one
-	var selected_universe_name = %WorldList.get_item_text(selected_index)
+	var selected_index = %UniverseList.get_selected_items()[0]  # This gives an array of selected item indices, [0] gets the first one
+	var selected_universe_name = %UniverseList.get_item_text(selected_index)
 	Game.current_universe_name = selected_universe_name
-	get_tree().change_scene_to_file("res://scenes/World/World.tscn")
+	get_tree().change_scene_to_file("res://scenes/Universe/Universe.tscn")
